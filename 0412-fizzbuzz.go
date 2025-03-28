e447ccbdb5b367ad917b413a4dfe472ec3bf248e
John David White <127981157+john-david3@users.noreply.github.com>
2025-03-28 12:55:15 +0000

    Add files via upload

---
+++ 0412-fizzbuzz.go
@@ -0,0 +1,20 @@
+package main
+
+import "strconv"
+
+func fizzBuzz(n int) []string {
+	res := make([]string, 0)
+	for i := range n {
+		i = i + 1
+		if i%3 == 0 && i%5 == 0 {
+			res = append(res, "FizzBuzz")
+		} else if i%3 == 0 {
+			res = append(res, "Fizz")
+		} else if i%5 == 0 {
+			res = append(res, "Buzz")
+		} else {
+			res = append(res, strconv.Itoa(i))
+		}
+	}
+	return res
+}
